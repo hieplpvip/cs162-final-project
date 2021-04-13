8cb2a2350a9a18a7900086b5fe82d8609c016f11
viethung9at1 <viethung.9at1@gmail.com>
2021-04-13 09:38:29 +0700

    Update new SchoolYear creating

--- src/SchoolYear.cpp
+++ src/SchoolYear.cpp
@@ -1,10 +1,14 @@
 #include "SchoolYear.h"
 #include "Global.h"
+#include"Semester.h"
 #include<iostream>
 using namespace std;
 void SchoolYear::createSchoolYear() {
-  SchoolYear sch;
+  SchoolYear *sch;
   cout<<"Input school year:";
-  cin>>sch.schoolYear;
+  cin>>sch->schoolYear;
   //More implement for Semester
+
+  //Add to global data
+  Global::all_school_year.push_back(sch);
 }
