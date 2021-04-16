5c031cd809e450983012636cb224df60660e0ef7
viethung9at1 <viethung.9at1@gmail.com>
2021-04-16 14:49:25 +0700

    Sone change on idea

--- src/Class.cpp
+++ src/Class.cpp
@@ -1,14 +1,13 @@
 #include "Class.h"
 #include "Global.h"
-#include<iostream>
+#include <iostream>
 using namespace std;
-void Class::createClass() {
-  Class cls;
-  cout<<"Input class id:";
-  cin>>cls.class_id;
-  cout<<"Input class name:";
-  cin>>cls.class_name;
-  cout<<"input the number of students:";
-  cin>>cls.numberOfStudent;
-  //more implement for student
+void Class::createClass()
+{
+  Class *cls;
+  cout << "Input class id:";
+  cin >> cls->class_id;
+  cout << "Input class name:";
+  cin >> cls->class_name;
+  Global::all_class.push_back(cls);
 }
