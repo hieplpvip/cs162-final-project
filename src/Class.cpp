0f8cecd27fea3ea148165fba2051c00eaea7fe82
viethung9at1 <viethung.9at1@gmail.com>
2021-04-18 18:38:28 +0700

    Complete view score board

--- src/Class.cpp
+++ src/Class.cpp
@@ -24,15 +24,28 @@ void Class::showScoreboard() {
     Class *cls = findClass(classID);
     cout << "Here is the score board of the class:\n";
     cout << setfill('*');
-    cout << setw(17) << '*' < < < endl;
+    cout << setw(117) << '*' << endl;
     for (int i = 0; i < cls->pStudents.size(); i++) {
         cout << "No." << i << endl;
         cout << "ID:" << cls->pStudents[i]->student_id << endl;
         cout << setw(12) << left << "Course ID:";
+        cout << setw(100) << left << "Course name";
         cout << setw(5) << left << "GPA:";
         cout << endl;
         for (int j = 0; j < cls->pStudents[i]->pEnrolledCourses.size(); j++) {
+            cout << setw(12) << left << cls->pStudents[i]->gpa_courses[j]->crs->course_id;
+            cout << setw(100) << left << cls->pStudents[i]->gpa_courses[j]->crs->course_name;
+            cout << setw(5) << left << cls->pStudents[i]->gpa_courses[j]->overallMark;
+            cout << endl;
         }
+        cout << setw(12) << left << "Semester";
+        cout << setw(5) << left << "GPA";
+        for (int j = 0; j < cls->pStudents[i]->gpa_semester.size(); i++) {
+            cout << setw(12) << left << cls->pStudents[i]->gpa_semester[j]->sms->ordinalOfSemester;
+            cout << setw(5) << left << cls->pStudents[i]->gpa_semester[j]->gpa;
+            cout << endl;
+        }
+        cout << endl;
     }
 }
 
--- src/Student.h
+++ src/Student.h
@@ -2,6 +2,7 @@
 #define STUDENT_H
 
 #include <string>
+
 #include "libs/Vector.h"
 
 using std::string;
@@ -12,28 +13,28 @@ class Course;
 class Semester;
 
 struct scorePerCourse {
-  Course *crs;
-  float overallGPA, midtermGMark, finalMark, otherMark;
+    Course *crs;
+    float midtermGMark, finalMark, otherMark, overallMark;
 };
 
 struct scorePerSemester {
-  Semester *sms;
-  float gpa;
+    Semester *sms;
+    float gpa;
 };
 
 class Student {
-public:
-  int student_id;
-  Vector<scorePerCourse*> gpa_courses;
-  Vector<scorePerSemester*> sms_courses;
-  float overallGPA;
-  string firstName, lastName, gender, dateOfBirth, socialID;
-  User *pUser;
-  Class *pClass;
-  Vector<Course *> pEnrolledCourses;
-  int numberOfEnrolledCourse;
-  static void createStudent();
-  static void showStudent();
+   public:
+    int student_id;
+    Vector<scorePerCourse *> gpa_courses;
+    Vector<scorePerSemester *> gpa_semester;
+    float overallGPA;
+    string firstName, lastName, gender, dateOfBirth, socialID;
+    User *pUser;
+    Class *pClass;
+    Vector<Course *> pEnrolledCourses;
+    int numberOfEnrolledCourse;
+    static void createStudent();
+    static void showStudent();
 };
 
 #endif
