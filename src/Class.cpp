fbcf32cd846c9040f66952938ee8919ddc3e8fea
viethung9at1 <viethung.9at1@gmail.com>
2021-04-18 23:26:55 +0700

    Revert "Uodate"
    This reverts commit 081e5afb5ae16156ad04f7df4c5ebd8de6aae0dd.

--- src/Class.cpp
+++ src/Class.cpp
@@ -2,25 +2,25 @@
 using namespace std;
 
 void Class::createClass() {
-    Class *cls = new Class();
-    cout << "Input class id:";
-    cin >> cls->class_id;
-    cout << "Input class name:";
-    cin >> cls->class_name;
-    Global::all_class.push_back(cls);
+  Class *cls = new Class();
+  cout << "Input class id:";
+  cin >> cls->class_id;
+  cout << "Input class name:";
+  cin >> cls->class_name;
+  Global::all_class.push_back(cls);
 }
 
 void Class::showClass() {
-    cout << "Here is a list of class:\n";
-    for (int i = 0; i < Global::all_class.size(); i++) {
-        cout << i << ":" << Global::all_class[i]->class_id << "-" << Global::all_class[i]->class_name << "-" << Global::all_class[i]->numberOfStudent << " students" << '\n';
-    }
+  cout << "Here is a list of class:\n";
+  for (int i = 0; i < Global::all_class.size(); i++) {
+    cout << i << ":" << Global::all_class[i]->class_id << "-" << Global::all_class[i]->class_name << "-" << Global::all_class[i]->numberOfStudent << " students"<<'\n';
+  }
 }
 
 void Class::showScoreboard() {
-    cout << "Here is the score board of the class:\n";
+  cout << "Here is the score board of the class:\n";
 }
 
 void Class::findClass(string classID) {
-    for (int i = 0;;)
+
 }
