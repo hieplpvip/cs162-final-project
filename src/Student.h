57279cbed3501cc8823d4780acfa8f16c75bfb22
viethung9at1 <viethung.9at1@gmail.com>
2021-04-18 20:46:25 +0700

    Update Student.h

--- src/Student.h
+++ src/Student.h
@@ -1,8 +1,8 @@
 #ifndef STUDENT_H
 #define STUDENT_H
 
-#include <iostream>
 #include <string>
+
 #include "libs/Vector.h"
 
 using std::string;
@@ -13,32 +13,28 @@ class Course;
 class Semester;
 
 struct scorePerCourse {
-  Course *crs;
-  float overallGPA, midtermGMark, finalMark, otherMark;
+    Course *crs;
+    float midtermGMark, finalMark, otherMark, overallMark;
 };
 
 struct scorePerSemester {
-  Semester *sms;
-  float gpa;
+    Semester *sms;
+    float gpa;
 };
 
 class Student {
-public:
-  int student_id;
-  Vector<scorePerCourse *> gpa_courses;
-  Vector<scorePerSemester *> sms_courses;
-  float overallGPA;
-  string firstName, lastName, gender, dateOfBirth, socialID;
-  User *pUser;
-  Class *pClass;
-  Vector<Course *> pEnrolledCourses;
-  int numberOfEnrolledCourse;
-
-  static void createStudent();
-  static void showStudent();
-
-  static Student *loadFromStream(std::istream &f);
-  void writeToStream(std::ostream &f);
+   public:
+    int student_id;
+    Vector<scorePerCourse *> gpa_courses;
+    Vector<scorePerSemester *> gpa_semester;
+    float overallGPA;
+    string firstName, lastName, gender, dateOfBirth, socialID;
+    User *pUser;
+    Class *pClass;
+    Vector<Course *> pEnrolledCourses;
+    int numberOfEnrolledCourse;
+    static void createStudent();
+    static void showStudent();
 };
 
 #endif
